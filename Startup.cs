aee9b3f8a8fcee72f658fdb99bf7b6609e32e8ee
Sajinpun <32304092+Sajinpun@users.noreply.github.com>
2019-05-18 15:20:42 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CricketPlayers.Startup))]
+namespace CricketPlayers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
